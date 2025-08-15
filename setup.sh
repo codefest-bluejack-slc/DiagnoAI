86e81e94bc35730b27f61cd6111c247b63d51f71
Steven Liementha <stevenliementha@gmail.com>
2025-08-15 16:02:06 +0700

    feat : add sh to run all setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,17 @@
+
+CANISTERS=("backend" "user")
+dfx stop
 dfx identity use default
 dfx start --clean --background
-npm run setup
+npm i
+for c in "${CANISTERS[@]}"; do
+    echo "Creating canister: $c"
+    dfx canister create "$c"
+    echo "Generating canister: $c"
+    dfx generate "$c"
+    echo "Deploying canister: $c"
+    dfx deploy "$c"
+done
+dfx canister create frontend
+dfx deploy frontend
 npm start
