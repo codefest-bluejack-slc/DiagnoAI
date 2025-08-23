0fcb910be9f6715474353994b9405ff2066ce2d2
Artificed <paultsai.id@gmail.com>
2025-08-23 13:25:22 +0700

    fix: Made recommendation agent response consistent

--- agents/diagnosis-agent/models/recommendation_agent_response.py
+++ agents/diagnosis-agent/models/recommendation_agent_response.py
@@ -1,7 +1,7 @@
 from typing import List
-from uagents import Model
+from uagents import Model, Field
 from models.medicine import Medicine
 
 class RecommendationAgentResponse(Model):
     answer: str
-    medicines: List[Medicine] = []
+    medicines: List[Medicine] = Field(default_factory=list)
